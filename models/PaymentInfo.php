f2e018e932a1864e19c3c9a8fc1cb9da902c107b
Luis Lutzen <luis.lutzen@gmail.com>
2019-04-24 13:11:19 +0200

    PaymentInfo.php Class added to models folder

---
+++ models/PaymentInfo.php
@@ -0,0 +1,263 @@
+<?php
+
+class PaymentInfo{
+    private $conn;
+    private $table ='payment_info';
+
+    //PaymentInfo properties
+    private $id;
+    private $firstName;
+    private $lastName;
+    private $address;
+    private $phoneNumber;
+    private $country;
+    private $zipCode;
+    private $email;
+    private $payment;
+    private $cardNumber;
+    private $cardExpirationDate;
+    private $cvcNumber;
+    private $userId;
+
+    /**
+     * @return mixed
+     */
+    public function getConn()
+    {
+        return $this->conn;
+    }
+
+    /**
+     * @param mixed $conn
+     */
+    public function setConn($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
+     * @param string $table
+     */
+    public function setTable($table)
+    {
+        $this->table = $table;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param mixed $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * @param mixed $zipCode
+     */
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayment()
+    {
+        return $this->payment;
+    }
+
+    /**
+     * @param mixed $payment
+     */
+    public function setPayment($payment)
+    {
+        $this->payment = $payment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * @param mixed $cardNumber
+     */
+    public function setCardNumber($cardNumber)
+    {
+        $this->cardNumber = $cardNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCardExpirationDate()
+    {
+        return $this->cardExpirationDate;
+    }
+
+    /**
+     * @param mixed $cardExpirationDate
+     */
+    public function setCardExpirationDate($cardExpirationDate)
+    {
+        $this->cardExpirationDate = $cardExpirationDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCvcNumber()
+    {
+        return $this->cvcNumber;
+    }
+
+    /**
+     * @param mixed $cvcNumber
+     */
+    public function setCvcNumber($cvcNumber)
+    {
+        $this->cvcNumber = $cvcNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+
+}
