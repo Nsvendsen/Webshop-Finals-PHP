05011493fd3dde2d36c566a75ec4450ebab52fc2
Nsvendsen <nilas@me.com>
2019-04-24 13:35:37 +0200

    user get/set

---
+++ models/Users.php
@@ -0,0 +1,98 @@
+<?php
+
+
+class Users{
+    private $conn;
+    private $table ='users';
+
+    //user properties
+    private $id;
+    private $firstName;
+    private $lastName;
+    private $address;
+    private $zipCode;
+    private $email;
+    private $confirmedEmail;
+    private $gender;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getConfirmedEmail()
+    {
+        return $this->confirmedEmail;
+    }
+
+    public function setConfirmedEmail($confirmedEmail)
+    {
+        $this->confirmedEmail = $confirmedEmail;
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+}
